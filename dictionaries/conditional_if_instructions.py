858e9555464afb214957da50df2d1a2b303f67f3
Jimmy <an.9647873111@gmail.com>
2024-05-20 00:21:12 +0200

    conditional_if_instructions

---
+++ dictionaries/conditional_if_instructions.py
@@ -0,0 +1,33 @@
+# example 1
+
+# my_number = 25
+
+# if my_number > 0:
+#     print(my_number, "is positive number")
+
+# ----
+
+# example 2
+
+# person_info = {
+#     'age': 20
+# }
+
+# if not person_info.get('name'):
+#     print("Name missing")
+
+# ----
+
+# example 3
+
+num_one = 10
+num_two = 5
+
+if (num_one > 0 and
+    num_two > 0 and
+    isinstance(num_one, int) and 
+    isinstance(num_two, int)):
+    print("Both numbers are ints and positive")
+
+
+
