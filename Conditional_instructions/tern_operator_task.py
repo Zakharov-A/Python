ee4be825341214d215053dda34deeca26820de77
Jimmy <an.9647873111@gmail.com>
2024-06-09 22:46:00 +0200

    Python_course

---
+++ Conditional_instructions/tern_operator_task.py
@@ -0,0 +1,32 @@
+# my_img = ('1920', '1080')
+
+# info = f"{my_img[0]}x{my_img[1]}" if len(
+#     my_img) == 2 else "Incorrect image formatting"
+
+# ----
+
+# my_img = ('2080', '1600')
+
+# print(f"{my_img[0]}x{my_img[1]}") if len(
+#     my_img) == 2 else print("Incorrect image formatting")
+
+# print(info)
+
+# Task 1
+
+# my_img = ('2080', '1600')
+
+# if len(my_img) == 2:
+#     info = f"{my_img[0]}x{my_img[1]}"
+# else:
+#     info = "Incorrect image formatting"    
+
+# print(info)
+
+ # Task 2
+
+my_str = "Very, very, very long string!"
+
+print("String is long" if len(my_str) > 40 else "String is short")
+   
+        
