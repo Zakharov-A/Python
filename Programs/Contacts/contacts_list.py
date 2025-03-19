bb66d6e6b3356e49e254cd7ff6927a77f0538783
Andrey Zakharov <88881099+Zakharov-A@users.noreply.github.com>
2025-03-19 23:55:26 +0300

    Python basic

--- Programs/Contacts/contacts_list.py
+++ Programs/Contacts/contacts_list.py
@@ -2,16 +2,29 @@ def main() -> None:
     contacts: dict[str, int] =  {"Petr": 111222, "Lena": 443322, "Andre": 556677}
     while True:
         command: str = input(""
-        "1 - Добавить\n"
+        "\n1 - Добавить\n"
         "2 - Удалить\n"
         "3 - Просмотр\n"
-        "4 - Выход из программы\n")
+        "4 - Выход из программы\n"
+        "\nВведите команду: ")
+        print()
         if command == '1':
-            name: str =input("Введите имя: ")
+            name: str = input("Введите имя: ")
+            if contacts.get(name):
+                print("Такое имя уже существует")
+                continue
             tel: int = int(input("Введите телефон: "))
             contacts[name] = tel
-        print(contacts)    
-
+        elif command == '2':
+            name: str = input("Введите имя: ")
+            if contacts.get(name):
+                contacts.pop(name)
+                print(f"Контакт {name} удален.")
+            else:
+                print(f"Имя {name} не найдено")
+        elif command == '3':
+            for name, phone in contacts.items():
+                print(f"{name} - {phone}")            
 
 
 main()    
