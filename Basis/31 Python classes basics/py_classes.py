647150960824aea5f3f62a283bce6d87e263ab63
Boba Fett <88881099+Zakharov-A@users.noreply.github.com>
2025-07-24 06:49:09 +0300

    Classes

--- Basis/31 Python classes basics/py_classes.py
+++ Basis/31 Python classes basics/py_classes.py
@@ -1,11 +1,41 @@
 class Animal:
-    def __init__(self, name, age):
+    def __init__(self, name, age, health=100, damage=0):
         self.name = name
-        self.age = age
+        self.__age = age
+        self.health = health
+        self.damage = damage
+
+    def set_name(self, value):
+        self.name = value
+
+    def get_name(self):
+        print(self.name)
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if value <= 10:
+            self.__age = value    
+
+    def __str__(self):
+        return f"Name - {self.name}, age - {self.__age}"     
+
+    def set_damage(self):
+        return self.damage
+    
+    def get_damage(self, value):
+        self.health -= value
 
 class Cat(Animal):
     def mau(self):
-        print('Mau-mau')    
+        print('Mau-mau')
+
+    def get_damage(self, value):
+        super().get_damage(value)
+        print(f"Maauuu; Health = {self.health}")        
 
 
 class Dog(Animal):
@@ -16,13 +46,17 @@ class Dog(Animal):
     def gav(self):
         print('Gav-gav')
 
+    def get_damage(self, value):
+        super().get_damage(value)
+        print(f"Aaauuu; Health = {self.health}")    
 
-cat = Cat('Bob', 2)
-print(cat.name, cat.age)
 
-dog = Dog('John', 'Wick', 3)
-print(dog.name, dog.lastname, dog.age)
+cat = Cat('Bob', 2, damage=10)
+dog = Dog('John', 'Wick', age=1, damage=15)
+
 dog.gav()
+cat.get_damage(dog.set_damage())
+cat.mau()
+dog.get_damage(cat.set_damage())
 
-dog2 = Dog(name = 'Bobik', lastname='Lee', age = 4)
-print(dog2.name, dog2.lastname)
+
