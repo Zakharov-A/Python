60c398c404333c15d5f72099ac8c527d21428a11
Boba Fett <88881099+Zakharov-A@users.noreply.github.com>
2025-11-20 06:25:33 +0300

    Python base

---
+++ TEST/first.py
@@ -0,0 +1,39 @@
+# print("Hello my friend")
+# print("How are you")
+# answer = input("Tell me about your plan for today: ")
+# print(answer)
+# print(type(answer))
+# snake_case
+# CamelCase
+# yet-another-package
+# CONSTANT
+
+# my_integer = 10
+# my_flout = 5.5
+# print(my_integer + my_flout)
+
+# x = 2
+# print(x ** 3)
+# y = 13
+# print(y // 2)
+# z = 9
+# print(z % 2)
+# a = 2 + 3 * 5
+# print(a)
+
+# big_number = 10 ** 1000
+# print(big_number)
+
+# flat_number = 34
+
+# entrance_number = (flat_number - 1) // 20 + 1
+# floor_number = (flat_number -1) % 20 // 4 + 1
+# print(entrance_number)
+# print(floor_number)
+
+# bool
+x = 10
+y = 10
+is_equal = x == y
+print(is_equal)
+# 00:27:00
